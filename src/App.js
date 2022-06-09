e60c0c6121947d068ee004f998d0c7129b727f94
Agustin Taborda <ataborda@Lenovo.com>
2022-06-08 23:25:49 -0300

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          welcome to <b>la caja magicalizadora</b> project
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/lacajamagicalizadora/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn more
         </a>
       </header>
     </div>
