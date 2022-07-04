a3ff31dfb8d015f0ee0c49d53f006639da5ca52b
Agustin Taborda <ataborda@Lenovo.com>
2022-07-04 00:09:50 -0300

    footer imp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import ItemListContainer from "./components/ItemListContainer";
 import Home from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/itemlist" element={<ItemListContainer />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
