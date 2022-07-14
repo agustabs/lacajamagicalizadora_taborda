5c678c4bf23161e4c823639042cc81b6253a9780
Agustin Taborda <ataborda@Lenovo.com>
2022-07-14 00:10:33 -0300

    cart details

--- src/components/CartDetails.jsx
+++ src/components/CartDetails.jsx
@@ -1,8 +1,35 @@
 import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useState } from "react";
 
 function CartDetails() {
   const location = useLocation();
   const { props } = location.state;
+  const [num, setNum] = useState(0);
+
+  const sumar = () => {
+    if (num < props.stock) {
+      setNum(num + 1);
+    } else {
+      Swal.fire({
+        title: "Whoops",
+        text: "Has alcanzado el máximo de unidades disponibles.",
+        icon: "error",
+        confirmButtonText: "Volver al catalogo",
+      });
+    }
+  };
+
+  const restar = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    }
+  };
+
+  const clear = () => {
+    setNum(0);
+  };
 
   return (
     <section className="text-gray-800 body-font overflow-hidden bg-sky-700">
@@ -14,7 +41,7 @@ function CartDetails() {
             src={props.img}
           ></img>
           <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h2 class="text-sm title-font text-gray-500 tracking-widest">
+            <h2 class="text-sm title-font text-gray-800 tracking-widest">
               La Caja Magicalizadora
             </h2>
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
@@ -77,7 +104,7 @@ function CartDetails() {
                 >
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <span className="text-gray-600 ml-3">4 Reviews</span>
+                <span className="text-gray-600 ml-3">4 Reseñas</span>
               </span>
               <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
                 <a className="text-gray-500">
@@ -132,25 +159,46 @@ function CartDetails() {
                 <p className="text-slate-400">{props.wineCellar}</p>
               </div>
             </div>
+            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+              <span className="w-full text-medium mb-5 text-gray-900">
+                Stock disponible: {props.stock}
+              </span>
+              <span className="w-full text-medium mb-5 text-gray-900">
+                Cantidad seleccionada: {num}
+              </span>
+            </div>
+            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+              <button
+                onClick={sumar}
+                className="w-full rounded-md bg-indigo-500  m-1 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75"
+              >
+                Agregar
+              </button>
+              <button
+                onClick={restar}
+                className="w-full rounded-md bg-indigo-500  m-1 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75"
+              >
+                Restar
+              </button>
+              <button
+                onClick={clear}
+                className="w-full rounded-md bg-indigo-500  m-1 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75"
+              >
+                Limpiar todo
+              </button>
+            </div>
             <div className="flex">
-              <span className="title-font font-medium text-2xl text-gray-900">
+              <span className="title-font font-medium text-2xl text-gray-900 p-3 m-1">
                 ${props.price}
               </span>
-              <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+              <button className="flex ml-auto text-white bg-indigo-500 border-0 p-3 m-1 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                 Agregar al carrito
               </button>
-              <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                <svg
-                  fill="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                </svg>
-              </button>
+              <Link to="/itemlist">
+                <button className="flex ml-auto text-white bg-indigo-500 border-0 p-3 m-1 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                  Volver al catalogo
+                </button>
+              </Link>
             </div>
           </div>
         </div>
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -38,27 +38,6 @@ function ItemCard(props) {
         />
         <div className="p-5">
           <p className="text-medium mb-5 text-gray-700">{props.name}</p>
-          <span className="w-full text-medium mb-5 text-gray-600">
-            Cantidad: {num}
-          </span>
-          <button
-            onClick={sumar}
-            className="w-full rounded-md bg-indigo-600  m-1 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75"
-          >
-            Agregar
-          </button>
-          <button
-            onClick={restar}
-            className="w-full rounded-md bg-indigo-600  m-1 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75"
-          >
-            Restar
-          </button>
-          <button
-            onClick={clear}
-            className="w-full rounded-md bg-indigo-600  m-1 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75"
-          >
-            Limpiar todo
-          </button>
           <Link to="/cartdetails" state={{ props }}>
             <button className="w-full rounded-md bg-indigo-600  m-1 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">
               Más información
